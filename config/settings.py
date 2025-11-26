2ea7451e48221b215a8132651063d8f91a730c3d
Mariaferrojas <mariaferrojas189@gmail.com>
2025-11-25 21:55:01 -0500

    :)

--- config/settings.py
+++ config/settings.py
@@ -11,8 +11,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 import csv
+from logging import config
 from pathlib import Path
-from decouple import config
+from decouple import config as env_config,Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -92,8 +93,11 @@ DATABASES = {
         'PORT': '3306',
     }
 }
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,.onrender.com')
-
+ALLOWED_HOSTS = env_config(
+    'ALLOWED_HOSTS',
+    default='localhost,127.0.0.1,.onrender.com',
+    cast=Csv()
+)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
