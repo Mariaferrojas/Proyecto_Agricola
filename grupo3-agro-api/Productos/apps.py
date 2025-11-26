ca77a6e933b9e1f60465ed35fc0997275950cc35
Andres0607MP <andresmancera06156@gmail.com>
2025-11-26 14:18:48 -0500

    elimino carpeta no necesaria

--- grupo3-agro-api/Productos/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-class ProductosConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Productos'
-    verbose_name = 'Gestión de Productos'
-    
-    def ready(self):
-        import Productos.signals
